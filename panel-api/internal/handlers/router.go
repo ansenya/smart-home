41556c552b332d54e2e83601c4150800f5b845ea
ansenya <ars.anoshin@gmail.com>
2026-01-02 17:36:42 +0300

    cors

--- panel-api/internal/handlers/router.go
+++ panel-api/internal/handlers/router.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"log/slog"
 	"panel-api/internal/config"
 )
 
@@ -11,17 +13,21 @@ type Router struct {
 
 	healthHandler *healthHandler
 	usersHandler  *usersHandler
+
+	log *slog.Logger
 }
 
-func NewRouter(container *config.Container) Router {
+func NewRouter(container *config.Container) *Router {
 	engine := gin.Default()
-
-	return Router{
+	router := Router{
 		engine:        engine,
 		config:        container,
 		healthHandler: newHealthHandler(),
 		usersHandler:  newUsersHandler(),
+		log:           container.Log,
 	}
+	router.configureCors()
+	return &router
 }
 
 func (r *Router) registerRoutes() {
@@ -31,4 +37,19 @@ func (r *Router) registerRoutes() {
 
 func (r *Router) Run() error {
 	return r.engine.Run(r.config.Server.Port)
+}
+
+func (r *Router) configureCors() {
+	r.log.Info("initializing cors")
+	r.engine.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"https://smarthome.hipahopa.ru",
+			"http://localhost:5173",
+		},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           86400,
+	}))
 }
